6c806df57cdd4fbc9c3a7d2f4eeabfcc281f4f69
DLehenbauer <DLehenbauer@users.noreply.github.com>
2017-03-21 07:31:06 -0700

    Declare remaining opcodes

--- src/compiler/transformers/wasm/binary.ts
+++ src/compiler/transformers/wasm/binary.ts
@@ -334,7 +334,205 @@ namespace ts.wasm {
     }
 
     export enum opcode {
+
+        // Control flow operators
+
+        unreachable     = 0x00,     //                                  trap immediately
+        nop             = 0x01,     //                                  no operation
+        block           = 0x02,     // sig : block_type                 begin a sequence of expressions, yielding 0 or 1 values
+        loop            = 0x03,     // sig : block_type                 begin a block which can also form control flow loops
+        if              = 0x04,     // sig : block_type                 begin if expression
+        else            = 0x05,     //                                  begin else expression of if
         end             = 0x0b,     //                                  end a block, loop, or if
+        br              = 0x0c,     // relative_depth : varuint32       break that targets an outer nested block
+        br_if           = 0x0d,     // relative_depth : varuint32       conditional break that targets an outer nested block
+        br_table        = 0x0e,     // see below                        branch table control flow construct
         return          = 0x0f,     //                                  return zero or one value from this function
+
+        // Call operators
+
+        call            = 0x10,     // function_index : varuint32                       call a function by its index
+        call_indirect   = 0x11,     // type_index : varuint32, reserved : varuint1      call a function indirect with an expected signature
+
+        // Parametric operators
+
+        drop            = 0x1a,     //                                  ignore value
+        select          = 0x1b,     //                                  select one of two values based on condition
+
+        get_local       = 0x20,     // local_index : varuint32          read a local variable or parameter
+        set_local       = 0x21,     // local_index : varuint32          write a local variable or parameter
+        tee_local       = 0x22,     // local_index : varuint32          write a local variable or parameter and return the same value
+        get_global      = 0x23,     // global_index : varuint32         read a global variable
+        set_global      = 0x24,     // global_index : varuint32         write a global variable
+
+        // Memory-related operators (described here)
+
+        i32_load        = 0x28,     // memory_immediate                 load from memory
+        i64_load        = 0x29,     // memory_immediate                 load from memory
+        f32_load        = 0x2a,     // memory_immediate                 load from memory
+        f64_load        = 0x2b,     // memory_immediate                 load from memory
+        i32_load8_s     = 0x2c,     // memory_immediate                 load from memory
+        i32_load8_u     = 0x2d,     // memory_immediate                 load from memory
+        i32_load16_s    = 0x2e,     // memory_immediate                 load from memory
+        i32_load16_u    = 0x2f,     // memory_immediate                 load from memory
+        i64_load8_s     = 0x30,     // memory_immediate                 load from memory
+        i64_load8_u     = 0x31,     // memory_immediate                 load from memory
+        i64_load16_s    = 0x32,     // memory_immediate                 load from memory
+        i64_load16_u    = 0x33,     // memory_immediate                 load from memory
+        i64_load32_s    = 0x34,     // memory_immediate                 load from memory
+        i64_load32_u    = 0x35,     // memory_immediate                 load from memory
+        i32_store       = 0x36,     // memory_immediate                 store to memory
+        i64_store       = 0x37,     // memory_immediate                 store to memory
+        f32_store       = 0x38,     // memory_immediate                 store to memory
+        f64_store       = 0x39,     // memory_immediate                 store to memory
+        i32_store8      = 0x3a,     // memory_immediate                 store to memory
+        i32_store16     = 0x3b,     // memory_immediate                 store to memory
+        i64_store8      = 0x3c,     // memory_immediate                 store to memory
+        i64_store16     = 0x3d,     // memory_immediate                 store to memory
+        i64_store32     = 0x3e,     // memory_immediate                 store to memory
+        current_memory  = 0x3f,     // reserved : varuint1              query the size of memory
+        grow_memory     = 0x40,     // reserved : varuint1              grow the size of memory
+
+        // Constants
+
+        i32_const       = 0x41,     // value : varint32                 a constant value interpreted as i32
+        i64_const       = 0x42,     // value : varint64                 a constant value interpreted as i64
+        f32_const       = 0x43,     // value : uint32                   a constant value interpreted as f32
+        f64_const       = 0x44,     // value : uint64                   a constant value interpreted as f64
+
+        // Comparison operators
+
+        i32_eqz         = 0x45,     // compare equal to zero (return 1 if operand is zero, 0 otherwise)
+        i32_eq          = 0x46,     // sign-agnostic compare equal
+        i32_ne          = 0x47,     // sign-agnostic compare unequal
+        i32_lt_s        = 0x48,     // signed less than
+        i32_lt_u        = 0x49,     // unsigned less than
+        i32_gt_s        = 0x4a,     // signed greater than
+        i32_gt_u        = 0x4b,     // unsigned greater than
+        i32_le_s        = 0x4c,     // signed less than or equal
+        i32_le_u        = 0x4d,     // unsigned less than
+        i32_ge_s        = 0x4e,     // signed greater than or equal
+        i32_ge_u        = 0x4f,     // unsigned greater than or equal
+        i64_eqz         = 0x50,     // compare equal to zero (return 1 if operand is zero, 0 otherwise)
+        i64_eq          = 0x51,     // sign-agnostic compare equal
+        i64_ne          = 0x52,     // sign-agnostic compare unequal
+        i64_lt_s        = 0x53,     // signed less than
+        i64_lt_u        = 0x54,     // unsigned less than
+        i64_gt_s        = 0x55,     // signed greater than
+        i64_gt_u        = 0x56,     // unsigned greater than
+        i64_le_s        = 0x57,     // signed less than or equal
+        i64_le_u        = 0x58,     // unsigned less than
+        i64_ge_s        = 0x59,     // signed greater than or equal
+        i64_ge_u        = 0x5a,     // unsigned greater than or equal
+        f32_eq          = 0x5b,     // compare ordered and equal
+        f32_ne          = 0x5c,     // compare unordered or unequal
+        f32_lt          = 0x5d,     // compare ordered and less than
+        f32_gt          = 0x5e,     // compare ordered and greater than
+        f32_le          = 0x5f,     // compare ordered and less than or equal
+        f32_ge          = 0x60,     // compare ordered and greater than or equal
+        f64_eq          = 0x61,     // compare ordered and equal
+        f64_ne          = 0x62,     // compare unordered or unequal
+        f64_lt          = 0x63,     // compare ordered and less than
+        f64_gt          = 0x64,     // compare ordered and greater than
+        f64_le          = 0x65,     // compare ordered and less than or equal
+        f64_ge          = 0x66,     // compare ordered and greater than or equal
+
+        // Numeric operators
+
+        i32_clz         = 0x67,     // sign-agnostic count leading zero bits (All zero bits are considered leading if the value is zero)
+        i32_ctz         = 0x68,     // sign-agnostic count trailing zero bits (All zero bits are considered trailing if the value is zero)
+        i32_popcnt      = 0x69,     // sign-agnostic count number of one bits
+        i32_add         = 0x6a,     // sign-agnostic addition
+        i32_sub         = 0x6b,     // sign-agnostic subtraction
+        i32_mul         = 0x6c,     // sign-agnostic multiplication (lower 32-bits)
+        i32_div_s       = 0x6d,     // signed division (result is truncated toward zero)
+        i32_div_u       = 0x6e,     // unsigned division (result is floored)
+        i32_rem_s       = 0x6f,     // signed remainder (result has the sign of the dividend)
+        i32_rem_u       = 0x70,     // unsigned remainder
+        i32_and         = 0x71,     // sign-agnostic bitwise and
+        i32_or          = 0x72,     // sign-agnostic bitwise inclusive or
+        i32_xor         = 0x73,     // sign-agnostic bitwise exclusive or
+        i32_shl         = 0x74,     // sign-agnostic shift left
+        i32_shr_s       = 0x75,     // zero-replicating (logical) shift right
+        i32_shr_u       = 0x76,     // sign-replicating (arithmetic) shift right
+        i32_rotl        = 0x77,     // sign-agnostic rotate left
+        i32_rotr        = 0x78,     // sign-agnostic rotate right
+        i64_clz         = 0x79,     // sign-agnostic count leading zero bits (All zero bits are considered leading if the value is zero)
+        i64_ctz         = 0x7a,     // sign-agnostic count trailing zero bits (All zero bits are considered trailing if the value is zero)
+        i64_popcnt      = 0x7b,     // sign-agnostic count number of one bits
+        i64_add         = 0x7c,     // sign-agnostic addition
+        i64_sub         = 0x7d,     // sign-agnostic subtraction
+        i64_mul         = 0x7e,     // sign-agnostic multiplication (lower 32-bits)
+        i64_div_s       = 0x7f,     // signed division (result is truncated toward zero)
+        i64_div_u       = 0x80,     // unsigned division (result is floored)
+        i64_rem_s       = 0x81,     // signed remainder (result has the sign of the dividend)
+        i64_rem_u       = 0x82,     // unsigned remainder
+        i64_and         = 0x83,     // sign-agnostic bitwise and
+        i64_or          = 0x84,     // sign-agnostic bitwise inclusive or
+        i64_xor         = 0x85,     // sign-agnostic bitwise exclusive or
+        i64_shl         = 0x86,     // sign-agnostic shift left
+        i64_shr_s       = 0x87,     // zero-replicating (logical) shift right
+        i64_shr_u       = 0x88,     // sign-replicating (arithmetic) shift right
+        i64_rotl        = 0x89,     // sign-agnostic rotate left
+        i64_rotr        = 0x8a,     // sign-agnostic rotate right
+        f32_abs         = 0x8b,     // absolute value
+        f32_neg         = 0x8c,     // negation
+        f32_ceil        = 0x8d,     // ceiling operator
+        f32_floor       = 0x8e,     // floor operator
+        f32_trunc       = 0x8f,     // round to nearest integer towards zero
+        f32_nearest     = 0x90,     // round to nearest integer, ties to even
+        f32_sqrt        = 0x91,     // square root
+        f32_add         = 0x92,     // addition
+        f32_sub         = 0x93,     // subtraction
+        f32_mul         = 0x94,     // multiplication
+        f32_div         = 0x95,     // division
+        f32_min         = 0x96,     // minimum (binary operator); if either operand is NaN, returns NaN
+        f32_max         = 0x97,     // maximum (binary operator); if either operand is NaN, returns NaN
+        f32_copysign    = 0x98,     // copysign
+        f64_abs         = 0x99,     // absolute value
+        f64_neg         = 0x9a,     // negation
+        f64_ceil        = 0x9b,     // ceiling operator
+        f64_floor       = 0x9c,     // floor operator
+        f64_trunc       = 0x9d,     // round to nearest integer towards zero
+        f64_nearest     = 0x9e,     // round to nearest integer, ties to even
+        f64_sqrt        = 0x9f,     // square root
+        f64_add         = 0xa0,     // addition
+        f64_sub         = 0xa1,     // subtraction
+        f64_mul         = 0xa2,     // multiplication
+        f64_div         = 0xa3,     // division
+        f64_min         = 0xa4,     // minimum (binary operator); if either operand is NaN, returns NaN
+        f64_max         = 0xa5,     // maximum (binary operator); if either operand is NaN, returns NaN
+        f64_copysign    = 0xa6,     // copysign
+
+        // Conversions
+
+        i32_wrap_i64        = 0xa7,     // wrap a 64-bit integer to a 32-bit integer
+        i32_trunc_s_f32     = 0xa8,     // truncate a 32-bit float to a signed 32-bit integer
+        i32_trunc_u_f32     = 0xa9,     // truncate a 32-bit float to an unsigned 32-bit integer
+        i32_trunc_s_f64     = 0xaa,     // truncate a 64-bit float to a signed 32-bit integer
+        i32_trunc_u_f64     = 0xab,     // truncate a 64-bit float to an unsigned 32-bit integer
+        i64_extend_s_i32    = 0xac,     // extend a signed 32-bit integer to a 64-bit integer
+        i64_extend_u_i32    = 0xad,     // extend an unsigned 32-bit integer to a 64-bit integer
+        i64_trunc_s_f32     = 0xae,     // truncate a 32-bit float to a signed 64-bit integer
+        i64_trunc_u_f32     = 0xaf,     // truncate a 32-bit float to an unsigned 64-bit integer
+        i64_trunc_s_f64     = 0xb0,     // truncate a 64-bit float to a signed 64-bit integer
+        i64_trunc_u_f64     = 0xb1,     // truncate a 64-bit float to an unsigned 64-bit integer
+        f32_convert_s_i32   = 0xb2,     // convert a signed 32-bit integer to a 32-bit float
+        f32_convert_u_i32   = 0xb3,     // convert an unsigned 32-bit integer to a 32-bit float
+        f32_convert_s_i64   = 0xb4,     // convert a signed 64-bit integer to a 32-bit float
+        f32_convert_u_i64   = 0xb5,     // convert an unsigned 64-bit integer to a 32-bit float
+        f32_demote_f64      = 0xb6,     // demote a 64-bit float to a 32-bit float
+        f64_convert_s_i32   = 0xb7,     // convert a signed 32-bit integer to a 64-bit float
+        f64_convert_u_i32   = 0xb8,     // convert an unsigned 32-bit integer to a 64-bit float
+        f64_convert_s_i64   = 0xb9,     // convert a signed 64-bit integer to a 64-bit float
+        f64_convert_u_i64   = 0xba,     // convert an unsigned 64-bit integer to a 64-bit float
+        f64_promote_f32     = 0xbb,     // promote a 32-bit float to a 64-bit float
+
+        // Reinterpretations
+
+        i32_reinterpret_f32 = 0xbc,     // reinterpret the bits of a 32-bit float as a 32-bit integer
+        i64_reinterpret_f64 = 0xbd,     // reinterpret the bits of a 64-bit float as a 64-bit integer
+        f32_reinterpret_i32 = 0xbe,     // reinterpret the bits of a 32-bit integer as a 32-bit float
+        f64_reinterpret_i64 = 0xbf,     // reinterpret the bits of a 64-bit integer as a 64-bit float
     }
 }
